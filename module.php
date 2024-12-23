7d7f11054149e23d765439f73f76bdbb749ff710
Herr Rossi <carpediem7878@gmx.de>
2024-12-24 00:36:22 +0100

    Use new webtrees registry container

--- module.php
+++ module.php
@@ -14,6 +14,7 @@ declare(strict_types=1);
 namespace HerrRossi\Webtrees;
 
 use Composer\Autoload\ClassLoader;
+use Fisharebest\Webtrees\Registry;
 use HerrRossi\Webtrees\DescendantFanChart\Module;
 
 // Register our namespace
@@ -22,4 +23,4 @@ $loader->addPsr4('HerrRossi\\Webtrees\\DescendantFanChart\\', __DIR__ . '/src');
 $loader->register();
 
 // Create and return instance of the module
-return app(Module::class);
+return Registry::container()->get(Module::class);
